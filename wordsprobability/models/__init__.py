5afa2a04fd8142c7148ed7c5594e1ea64143a6b8
meiri.yoav (laccl-srv1) <meiri.yoav@campus.technion.ac.il>
2024-10-07 17:35:21 +0300

    rename and format model names

--- wordsprobability/models/__init__.py
+++ wordsprobability/models/__init__.py
@@ -1,26 +1,36 @@
-from .bow_lm import \
-    EnglishGpt2Small, EnglishGpt2Medium, EnglishGpt2Large, EnglishGpt2Xl, \
-    EnglishPythia70M, EnglishPythia160M, EnglishPythia410M, \
-    EnglishPythia14B, EnglishPythia28B, EnglishPythia69B, EnglishPythia120B
+from .bow_lm import (
+    EnglishGpt2Small,
+    EnglishGpt2Medium,
+    EnglishGpt2Large,
+    EnglishGpt2Xl,
+    EnglishPythia70M,
+    EnglishPythia160M,
+    EnglishPythia410M,
+    EnglishPythia14B,
+    EnglishPythia28B,
+    EnglishPythia69B,
+    EnglishPythia120B,
+)
 
 MODELS = {
     "gpt2-small": EnglishGpt2Small,
     "gpt2-medium": EnglishGpt2Medium,
     "gpt2-large": EnglishGpt2Large,
     "gpt2-xl": EnglishGpt2Xl,
-    "pythia-70m": EnglishPythia70M,
-    "pythia-160m": EnglishPythia160M,
-    "pythia-410m": EnglishPythia410M,
-    "pythia-14b": EnglishPythia14B,
-    "pythia-28b": EnglishPythia28B,
-    "pythia-69b": EnglishPythia69B,
-    "pythia-120b": EnglishPythia120B,
+    "EleutherAI/pythia-70m": EnglishPythia70M,
+    "EleutherAI/pythia-160m": EnglishPythia160M,
+    "EleutherAI/pythia-410m": EnglishPythia410M,
+    "EleutherAI/pythia-1.4b": EnglishPythia14B,
+    "EleutherAI/pythia-2.8b": EnglishPythia28B,
+    "EleutherAI/pythia-6.9b": EnglishPythia69B,
+    "EleutherAI/pythia-12b": EnglishPythia120B,
 }
 
 
-def get_model(model_name):
+def get_model(model_name, model, tokenizer):
     model_cls = MODELS[model_name]
-    return model_cls()
+    return model_cls(model=model, tokenizer=tokenizer)
+
 
 def get_bow_symbol(model_name):
     return MODELS[model_name].bow_symbol
