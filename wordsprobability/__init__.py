56556ec217866f5e96b28ffd2deee50c5b2b1d1a
meiri.yoav (laccl-srv1) <meiri.yoav@campus.technion.ac.il>
2024-10-07 12:30:11 +0300

    ruff format, main example and cpu enforcement

--- wordsprobability/__init__.py
+++ wordsprobability/__init__.py
@@ -1,4 +1,2 @@
-from .main import get_surprisal_per_word
-
-__version__ = '0.17'
+__version__ = "0.17"
 __author__ = "Tiago Pimentel and Clara Meister"
--- wordsprobability/main.py
+++ wordsprobability/main.py
@@ -2,19 +2,23 @@ import argparse
 from typing import List, Optional
 import pandas as pd
 
-from .models import get_model, get_bow_symbol
-from .utils import constants, utils
+from text_metrics.pimentel_word_prob.wordsprobability.models import (
+    get_model,
+    get_bow_symbol,
+)
+from text_metrics.pimentel_word_prob.wordsprobability.utils import constants
+import os
 
 
 def get_args():
     parser = argparse.ArgumentParser()
     # Data files
-    parser.add_argument('--input', type=str, required=True)
-    parser.add_argument('--output', type=str, required=True)
+    parser.add_argument("--input", type=str, required=True)
+    parser.add_argument("--output", type=str, required=True)
     # Model
-    parser.add_argument('--model', type=str, required=True, choices=constants.MODELS)
+    parser.add_argument("--model", type=str, required=True, choices=constants.MODELS)
     # Extra Options
-    parser.add_argument('--return-buggy-surprisals', action='store_true')
+    parser.add_argument("--return-buggy-surprisals", action="store_true")
 
     return parser.parse_args()
 
@@ -27,78 +31,89 @@ def get_surprisals_per_subword(text, model_name):
         results, offsets = model.get_predictions(utterance.strip())
 
         df = pd.DataFrame(results)
-        df['text_id'] = text_id
-        df['offsets'] = offsets
+        df["text_id"] = text_id
+        df["offsets"] = offsets
         dfs += [df]
 
     return pd.concat(dfs)
 
 
 def mark_bos_subwords(df):
-    df['ones'] = 1
-    df['text_pos'] = df.groupby('text_id')['ones'].cumsum()
-    df['is_bos'] = df['text_pos'] == 1
-    del df['ones']
-    del df['text_pos']
+    df["ones"] = 1
+    df["text_pos"] = df.groupby("text_id")["ones"].cumsum()
+    df["is_bos"] = df["text_pos"] == 1
+    del df["ones"]
+    del df["text_pos"]
 
     return df
 
 
 def mark_eow_subwords(df):
-    df['is_eow'] = df.groupby('text_id')['is_bow'].shift(-1)
-    df.loc[df['is_eow'].isna(), 'is_eow'] = True
+    df["is_eow"] = df.groupby("text_id")["is_bow"].shift(-1)
+    df.loc[df["is_eow"].isna(), "is_eow"] = True
 
     return df
 
 
-def agg_surprisal_per_word(df: pd.DataFrame, model_name: str,
-                           return_buggy_surprisals: Optional[bool] = False) -> pd.DataFrame:
+def agg_surprisal_per_word(
+    df: pd.DataFrame, model_name: str, return_buggy_surprisals: Optional[bool] = False
+) -> pd.DataFrame:
     bow_symbol = get_bow_symbol(model_name)
 
-    df['is_bow'] = df.subword.apply(lambda x: x[0] == bow_symbol)
+    df["is_bow"] = df.subword.apply(lambda x: x[0] == bow_symbol)
     df = mark_bos_subwords(df)
     df = mark_eow_subwords(df)
-    df['word_id'] = df.groupby('text_id')['is_bow'].cumsum()
+    df["word_id"] = df.groupby("text_id")["is_bow"].cumsum()
 
-    df['surprisal_buggy'] = df['surprisal']
-    df['surprisal'] = df['surprisal'] \
-                      - df['bow_fix'] * df['is_bow'] \
-                      - df['bos_fix'] * df['is_bos'] \
-                      + df['eow_fix'] * df['is_eow']
+    df["surprisal_buggy"] = df["surprisal"]
+    df["surprisal"] = (
+        df["surprisal"]
+        - df["bow_fix"] * df["is_bow"]
+        - df["bos_fix"] * df["is_bos"]
+        + df["eow_fix"] * df["is_eow"]
+    )
 
-    df_per_word = df.groupby(['text_id', 'word_id']).agg('sum')
+    df_per_word = df.groupby(["text_id", "word_id"]).agg("sum")
 
     assert ((df_per_word.is_bow + df_per_word.is_bos) == 1).all()
     assert (df_per_word.is_eow == 1).all()
 
-    df_per_word['word'] = df_per_word.subword.apply(
-        lambda x: x[1:] if (x[0] == bow_symbol) else x)
+    df_per_word["word"] = df_per_word.subword.apply(
+        lambda x: x[1:] if (x[0] == bow_symbol) else x
+    )
 
-    return_columns = ['word', 'surprisal', 'surprisal_buggy'] \
-        if return_buggy_surprisals else ['word', 'surprisal']
+    return_columns = (
+        ["word", "surprisal", "surprisal_buggy"]
+        if return_buggy_surprisals
+        else ["word", "surprisal"]
+    )
     return df_per_word[return_columns]
 
 
-def _get_surprisal_per_word(text_list: List[str], model_name: str,
-                            return_buggy_surprisals: Optional[bool] = False) -> pd.DataFrame:
+def _get_surprisal_per_word(
+    text_list: List[str],
+    model_name: str,
+    return_buggy_surprisals: Optional[bool] = False,
+) -> pd.DataFrame:
     df = get_surprisals_per_subword(text_list, model_name)
     return agg_surprisal_per_word(df, model_name, return_buggy_surprisals)
 
 
-def get_surprisal_per_word(text: str, model_name: str,
-                           return_buggy_surprisals: Optional[bool] = False) -> pd.DataFrame:
-    text_list = text.split('\n')
+def get_surprisal_per_word(
+    text: str, model_name: str, return_buggy_surprisals: Optional[bool] = False
+) -> pd.DataFrame:
+    text_list = text.split("\n")
     return _get_surprisal_per_word(text_list, model_name, return_buggy_surprisals)
 
 
 def main():
-    args = get_args()
+    # set cuda visible devices to 1
 
-    text = utils.read_txt(args.input)
-    df = _get_surprisal_per_word(text, args.model,
-                                 return_buggy_surprisals=args.return_buggy_surprisals)
-    utils.write_tsv(df, args.output)
+    os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+    text = ["Hi, my name is John. I like playing sports."]
+    df = _get_surprisal_per_word(text, "pythia-70m", return_buggy_surprisals=True)
+    print(df)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
